e3bf606b00f25c5fe87633eaf0d2cc293bddf08d
JakeKViti <thevitij64@gmail.com>
2020-08-19 00:29:04 -0400

    implemented getting seasons from years 1950-79

--- lib/project/api.rb
+++ lib/project/api.rb
@@ -1,26 +1,34 @@
 class Project::API
     def self.get_seasons
         seasons = RestClient.get('http://ergast.com/api/f1/seasons.json') 
-        @seasons = JSON.parse(seasons)   
+        #seasons1 = RestClient.get('http://ergast.com/api/f1/seasons?limit=30&offset=30.json') 
+        @seasons = JSON.parse(seasons) 
+        #@seasons1 = JSON.parse(seasons1)
+       #binding.pry  
         self.get_season_table 
+        @test.each do |key, value|
+            puts "Key = #{key} / Value = #{value}"
+        Project::Seasons.new_seasons_from_API(key)
+        end
     end
 
 
     def self.get_season_table
         seasonnum = 1
         @seasons.each do |key, value|
-            @test = value 
+            @MRData = value 
         end
-        @test.each do |key, value|
+        @MRData.each do |key, value|
             if key == "SeasonTable"
-            @seasonTable = value
+            @seasontable = key
+            @season = value
             end
         end
-        @seasonTable.each do |key, value|
-            puts "key is #{key}"
-            puts "value is #{value}"
-            puts "seasonnum is #{seasonnum}"
-            seasonnum += 1
+        @season.each do |key, value|
+            @test = value
+           
+            #puts "Key = #{key} / Value = #{value}"
         end
+
     end
 end
--- lib/project/seasons.rb
+++ lib/project/seasons.rb
@@ -1,15 +1,35 @@
 class Project::Seasons
     attr_accessor :season, :seasonUrl
 
+    @@all = []
+
     def self.new_seasons_from_API(seasons)
         seasons.each do |key, value|
-               self.new(key, value)
+            self.checker(key, value)
         end
     end
 
     def initialize(season=nil, seasonUrl=nil)
         @season = season
         @seasonUrl = seasonUrl
-        binding.pry
+        @@all << self
     end
+
+    def self.checker(key, value)
+        if key == "season"
+            @number = value
+        elsif key == "url"
+            @url = value
+        end
+         if @number != nil && @url != nil
+            self.new(@number, @url)
+            @number = nil
+            @url = nil
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
 end
