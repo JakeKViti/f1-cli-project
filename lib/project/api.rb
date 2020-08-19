b85582a90b16b09b00e046dda3db001f37bc6338
JakeKViti <thevitij64@gmail.com>
2020-08-19 00:41:04 -0400

    cleaned up comments and changed test names to make more sence

--- lib/project/api.rb
+++ lib/project/api.rb
@@ -3,29 +3,23 @@ class Project::API
         seasons = RestClient.get('http://ergast.com/api/f1/seasons.json?limit=100') 
         @seasons = JSON.parse(seasons) 
         self.get_season_table 
-        @test.each do |key, value|
-            puts "Key = #{key} / Value = #{value}"
+        @seasonList.each do |key, value|
         Project::Seasons.new_seasons_from_API(key)
         end
     end
 
 
     def self.get_season_table
-        seasonnum = 1
         @seasons.each do |key, value|
             @MRData = value 
         end
         @MRData.each do |key, value|
             if key == "SeasonTable"
-            @seasontable = key
-            @season = value
+            @seasonTable = value
             end
         end
-        @season.each do |key, value|
-            @test = value
-           
-            #puts "Key = #{key} / Value = #{value}"
+        @seasonTable.each do |key, value|
+            @seasonList = value
         end
-
     end
 end
