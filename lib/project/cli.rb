3dfc39268098a5ce24ee9222959414a599f98625
JakeKViti <thevitij64@gmail.com>
2020-08-20 22:24:58 -0400

    starting to setup cli

--- lib/project/cli.rb
+++ lib/project/cli.rb
@@ -1,8 +1,9 @@
 class Project::CLI
     def start
+        puts "Welcome to F1 API!"
+        puts "Loading! Please wait!"
         Project::API.get_seasons()
         Project::API.get_circuts()
-        puts "Welcome to F1 API!"
         list_options
         user_choice
     end
@@ -12,6 +13,7 @@ class Project::CLI
         puts "Enter 'q' to see what tracks were run in a desired year"
         puts "Enter 'w' to see all tracks"
         puts "Enter 'e' to see all years"
+        puts "Enter 'p' to debug "
         puts "Enter 'x' to end the code"
     end
 
@@ -22,30 +24,50 @@ class Project::CLI
         case action
         when "q"
             puts "What year would you like to see?"
-            getYear = gets.strip
-            Project::API.get_year(getYear)
-            getYear = nil
-            list_options
+            input_checker
+
+            
         when "w"
-            puts Project::Circuit.all
+            counter = 1
+            sorted = Project::Circuit.all.sort_by do |circuts|
+                circuts.circuit
+            end
+            sorted.each do |circuts|
+                puts "#{counter}. #{circuts.circuit}"
+                counter += 1   
+            end
+            list_options
         when "e"
-            puts Project::Year.all
+            sorted = Project::Seasons.all.sort_by do |years|
+                years.season
+            end
+            puts "Here is every year a World Championship was held"
+            sorted.each do |years|
+                puts "#{years.season}"                
+            end
+            list_options
+        when "p"
+            puts "Currently working on nothing new"
         end
     end
         #asks for what year they want
         #sends to input for response
     end
 
-    #def inputer
-        #asks for input
-        #checks to see if input works
-        #if checker is good mark flag good & go back to list options
-        #if "exit" send a goodbye message and end program
-    #end   
-
-    #def input checker
-        #checks input
-    #end
+    def input_checker()
+        getYear = gets.to_i
+        if getYear < 1950
+            puts "Sorry, but the 'FIA World Championship of Drivers' was not yet competed for. While the first race ran was in 1947, the modern day Formula 1 started in 1950"
+            puts "Please enter another year!"
+            input_checker()
+        elsif getYear > 2021
+            puts "This season has not happened yet!"
+            input_checker
+        end
+        Project::API.get_year(getYear)
+            getYear = nil
+            list_options
+    end
 end
 
 
