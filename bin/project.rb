c6605e8c8d37f6af4fff01140fee0c499ba2ad1d
JakeKViti <thevitij64@gmail.com>
2020-08-25 01:14:42 -0400

    Commented api.rb and project.rb in bin

--- bin/project.rb
+++ bin/project.rb
@@ -2,4 +2,4 @@
 
 require_relative '../lib/project'
 
-Project::CLI.new.start
+Project::CLI.new.start #This starts the program
--- lib/project/api.rb
+++ lib/project/api.rb
@@ -1,16 +1,16 @@
 class Project::API
-    def self.get_seasons
+    def self.get_seasons #This loads all the seasons
         seasons = RestClient.get('http://ergast.com/api/f1/seasons.json?limit=1000') 
         @seasons = JSON.parse(seasons) 
         self.get_season_table 
         @seasonList.each do |key, value|
-        Project::Seasons.new_seasons_from_API(key)
+        Project::Seasons.new_seasons_from_API(key) #Sends seasons number to Seasons class
         end
     end
 
 
-    def self.get_season_table
-        @seasons.each do |key, value|
+    def self.get_season_table #We have to go through 4 steps into a hash, that what this method does
+        @seasons.each do |key, value| 
             @MRData = value 
         end
         @MRData.each do |key, value|
@@ -28,11 +28,11 @@ class Project::API
         @circuts = JSON.parse(circuts) 
         self.get_circut_table
         @circuitList.each do |key, value|
-            Project::Circuit.new_circuit_from_API(key)
+            Project::Circuit.new_circuit_from_API(key) #sends the hash from get_circuit_table into the circuit class
         end
     end
 
-    def self.get_circut_table
+    def self.get_circut_table  #We have to go through 4 steps into a hash, that what this method does
         @circuts.each do |key, value|
             @CircutData = value 
         end
@@ -49,13 +49,13 @@ class Project::API
     end
 
     def self.get_year(year)
-        season = RestClient.get("http://ergast.com/api/f1/#{year}.json?limit=52")
+        season = RestClient.get("http://ergast.com/api/f1/#{year}.json?limit=52") #user year gets put into here
         @year = JSON.parse(season)    
         self.get_year_table
-        Project::Year.years_from_API(@yearList)
+        Project::Year.years_from_API(@yearList) #sends a hash with years and tracks to year class
     end
 
-    def self.get_year_table
+    def self.get_year_table #Goes a few steps to a hash go get another hash to send to another class
         @year.each do |key, value|
             @yearData = value 
         end
